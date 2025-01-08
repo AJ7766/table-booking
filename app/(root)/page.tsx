218bbe7e4b43b46e75192827afd01ce8a5baf78b
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-01-08 23:43:14 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import { BookingFormX } from "./components/BookingForm";
+import { BookingForm } from "./components/BookingFormX";
 import { handleGetBookings } from "./services/formHandlers";
 
 export default async function Home() {
   const bookedTimesData = await handleGetBookings();
   return (
     <main className="h-full flex pt-10">
-      <BookingFormX bookedTimes={bookedTimesData} />
+      <BookingForm bookedTimes={bookedTimesData} />
     </main>
   );
 }
