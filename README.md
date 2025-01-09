3bef018f5c66063a26af94fe6bd73efe1360312a
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-01-09 01:50:31 +0100

    comments updated

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+## Short summary
+-- It has dark / lightmode 😎
+#User facing
+-- Make a booking
+-- Update their booking by entering their email again
+-- A specific time in the day is limited to one table, which means only one person can book '01-02-2025 at 18.00' therefore I set the Date to @unique, for scalable purposes this can be adjusted
+-- A user cant book a table if the time has past 'current time', example if current time is 18.32 the user cant book anything past 18.32
+-- A user can only book 3 weeks ahead
+#Admin facing
+-- Logs in through a local account using Baisc Authentication, could make a Admin model in the database for scalable purposes
+-- Cancel a booking, even tho if a table is booked it's already hidden on the client, toggling this won't really affect the client
+-- Delete a booking, will make the deleted booking show up again on client
+-- Pagination
+
+
 ## Install
 npm install
 
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -22,53 +22,56 @@ export default async function AdminPage({ searchParams }: { searchParams: Promis
     const { page, per_page, start, end, date } = await getSearchParams({ searchParams })
     const { bookings, totalBookings } = await handleGetFilteredBookings({ start, end, date });
     return (
-        <main className="h-full w-full grid grid-cols-1 pt-10 md:px-10 px-0">
-            <div className="overflow-auto">
-                <Table className="space-y-4 md:m-auto mx-4 my-4">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Name</TableHead>
-                            <TableHead>Email</TableHead>
-                            <TableHead>Guests</TableHead>
-                            <TableHead><Calendar /></TableHead>
-                            <TableHead>Canceled</TableHead>
-                            <TableHead>Edit</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {bookings.map((booking) => (
-                            <TableRow key={booking.id}>
-                                <TableCell className="whitespace-nowrap">{booking.name}</TableCell>
-                                <TableCell className="whitespace-nowrap">{booking.email}</TableCell>
-                                <TableCell className="whitespace-nowrap">{booking.guests}</TableCell>
-                                <TableCell className="whitespace-nowrap">{formatDateTimeReadable(booking.dateTime)}</TableCell>
-                                <TableCell className="whitespace-nowrap">{booking.canceled ? 'Yes' : 'No'}</TableCell>
-                                <TableCell>
-                                    <DropdownMenu>
-                                        <DropdownMenuTrigger>
-                                            <MoreVertical />
-                                            <span className="sr-only">Actions</span>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent>
-                                            <ToggleCancelDropdownItem id={booking.id} canceled={booking.canceled} />
-                                            <DeleteDropdownItem id={booking.id} />
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                </TableCell>
+        <>
+            <main className="h-full w-full flex flex-col md:gap-5 gap-1 justify-center pt-10 md:px-10 px-0">
+                <h1 className="md:m-0 text-xl mx-4 mt-4 mb-0">Bookings</h1>
+                <div className="overflow-x-auto overflow-y-hidden">
+                    <Table className="space-y-4 md:m-auto mx-4 my-4">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Email</TableHead>
+                                <TableHead>Guests</TableHead>
+                                <TableHead><Calendar /></TableHead>
+                                <TableHead>Canceled</TableHead>
+                                <TableHead>Edit</TableHead>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
-                    </TableFooter>
-                </Table >
-                {bookings.length < 1 && <div className="m-5">No bookings found</div>}
-            </div>
-            <Pagination
-                page={page}
-                per_page={per_page}
-                hasPrevPage={start > 1}
-                hasNextPage={end < totalBookings}
-            />
-        </main>
+                        </TableHeader>
+                        <TableBody>
+                            {bookings.map((booking) => (
+                                <TableRow key={booking.id}>
+                                    <TableCell className="whitespace-nowrap">{booking.name}</TableCell>
+                                    <TableCell className="whitespace-nowrap">{booking.email}</TableCell>
+                                    <TableCell className="whitespace-nowrap">{booking.guests}</TableCell>
+                                    <TableCell className="whitespace-nowrap">{formatDateTimeReadable(booking.dateTime)}</TableCell>
+                                    <TableCell className="whitespace-nowrap">{booking.canceled ? 'Yes' : 'No'}</TableCell>
+                                    <TableCell>
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger>
+                                                <MoreVertical />
+                                                <span className="sr-only">Actions</span>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent>
+                                                <ToggleCancelDropdownItem id={booking.id} canceled={booking.canceled} />
+                                                <DeleteDropdownItem id={booking.id} />
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                        </TableFooter>
+                    </Table >
+                    {bookings.length < 1 && <div className="m-5">No bookings found</div>}
+                </div>
+                <Pagination
+                    page={page}
+                    per_page={per_page}
+                    hasPrevPage={start > 1}
+                    hasNextPage={end < totalBookings}
+                />
+            </main>
+        </>
     )
 }
